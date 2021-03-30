688e511b153a08fee38091f55fa8ac4e8e156fa2
mr-r4ms0n <71031286+mr-r4ms0n@users.noreply.github.com>
2021-03-29 18:52:55 -0600

    figurasv1

---
+++ Paintv1/src/canvas/JavaDraw2DPanelM.java
@@ -0,0 +1,339 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package canvas;
+
+import figuras.C1;
+import figuras.Casa;
+import figuras.Diamante;
+import figuras.Triangulo;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.Polygon;
+import java.awt.Rectangle;
+import java.awt.RenderingHints;
+import java.awt.Shape;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.geom.Arc2D;
+import java.awt.geom.CubicCurve2D;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
+import java.awt.geom.QuadCurve2D;
+import java.awt.geom.RoundRectangle2D;
+import java.util.Vector;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author kenit
+ */
+public class JavaDraw2DPanelM extends JPanel implements MouseListener, MouseMotionListener
+{
+
+    private Vector shapes = new Vector();
+    public static final int RECTANGLE = 0;
+    public static final int ROUNDRECTANGLE2D = 1;
+    public static final int ELLIPSE2D = 2;
+    public static final int ARC2D = 3;
+    public static final int LINE2D = 4;
+    public static final int QUADCURVE2D = 5;
+    public static final int CUBICCURVE2D = 6;
+    public static final int POLYGON = 7;
+    public static final int GENERAL = 8;
+    public static final int AREA = 9;
+
+    //Figuras propias
+    public static final int HEART = 10;
+    public static final int HOUSE = 11;
+    public static final int BAT = 12;
+    public static final int TRIANGLE = 13;
+    public static final int DIAMOND = 14;
+
+    public int shapeType = RECTANGLE;
+    // vector of input points
+    Vector points = new Vector();
+    int pointIndex = 0;
+    Shape partialShape = null;
+    Point p = null;
+
+    //Coordenadas para dibujar el plano cartesiano
+    int alto = 1370;
+    int ancho = 654;
+
+    public JavaDraw2DPanelM()
+    {
+        super();
+        setBackground(Color.white);
+        setPreferredSize(new Dimension(1155, 504));
+        addMouseListener(this);
+        addMouseMotionListener(this);
+        System.out.println("Entre");
+    }
+
+    public void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+
+        g.setColor(Color.black);
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        for (int i = 0; i < shapes.size(); i++)
+        {
+            Shape s = (Shape) shapes.get(i);
+            g2.draw(s);
+        }
+    }
+
+    public void mouseClicked(MouseEvent ev)
+    {
+    }
+
+    public void mouseEntered(MouseEvent ev)
+    {
+    }
+
+    public void mouseExited(MouseEvent ev)
+    {
+    }
+
+    public void mousePressed(MouseEvent ev)
+    {
+        points.add(ev.getPoint());
+        pointIndex++;
+        p = null;
+    }
+
+    public void mouseReleased(MouseEvent ev)
+    {
+        Graphics g = getGraphics();
+        Point p1 = (Point) (points.get(pointIndex - 1));
+        p = ev.getPoint();
+        Shape s = null;
+        switch (shapeType)
+        {
+            case RECTANGLE:
+                s = new Rectangle(p1.x, p1.y, p.x - p1.x, p.y - p1.y);
+                break;
+            case ROUNDRECTANGLE2D:
+                s = new RoundRectangle2D.Float(p1.x, p1.y, p.x - p1.x, p.y - p1.y, 10, 10);
+                break;
+            case ELLIPSE2D:
+                s = new Ellipse2D.Float(p1.x, p1.y, p.x - p1.x, p.y - p1.y);
+                break;
+            case ARC2D:
+                s = new Arc2D.Float(p1.x, p1.y, p.x - p1.x,
+                        p.y - p1.y,
+                        30, 120, Arc2D.OPEN
+                );
+                break;
+            case LINE2D:
+                s = new Line2D.Float(p1.x, p1.y, p.x, p.y);
+                break;
+            case QUADCURVE2D:
+                if (pointIndex > 1)
+                {
+                    Point p2 = (Point) points.get(0);
+                    s = new QuadCurve2D.Float(p2.x, p2.y, p1.x, p1.y, p.x, p.y);
+                }
+                break;
+            case CUBICCURVE2D:
+                if (pointIndex > 2)
+                {
+                    Point p2 = (Point) points.get(pointIndex - 2);
+                    Point p3 = (Point) points.get(pointIndex - 3);
+                    s = new CubicCurve2D.Float(p3.x, p3.y, p2.x, p2.y,
+                            p1.x, p1.y, p.x, p.y);
+                }
+                break;
+            case POLYGON:
+                if (ev.isShiftDown())
+                {
+                    s = new Polygon();
+                    for (int i = 0; i < pointIndex; i++)
+                    {
+                        ((Polygon) s).addPoint(((Point) points.get(i)).x,
+                                ((Point) points.get(i)).y);
+                    }
+                    ((Polygon) s).addPoint(p.x, p.y);
+                }
+                break;
+            case HEART:
+                s = new C1(p1.x, p1.y, p.x - p1.x, p.y - p1.y);
+                break;
+            case HOUSE:
+                s = new Casa(p1.x, p1.y, p.x - p1.x, p.y - p1.y);
+                break;
+            case TRIANGLE:
+                s = new Triangulo(p1.x, p1.y, p.x - p1.x, p.y - p1.y);
+                break;
+            case DIAMOND:
+                s = new Diamante(p1.x, p1.y, p.x - p1.x, p.y - p1.y);
+                break;
+        }
+        if (s != null)
+        {
+            shapes.add(s);
+            points.clear();
+            pointIndex = 0;
+            p = null;
+            repaint();
+        }
+    }
+
+    public void mouseMoved(MouseEvent ev)
+    {
+    }
+
+    public void mouseDragged(MouseEvent ev)
+    {
+        Graphics2D g = (Graphics2D) getGraphics();
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g.setXORMode(Color.white);
+        Point p1 = (Point) points.get(pointIndex - 1);
+        switch (shapeType)
+        {
+            case RECTANGLE:
+                if (p != null)
+                {
+                    g.drawRect(p1.x, p1.y, p.x - p1.x, p.y - p1.y);
+                }
+                p = ev.getPoint();
+                g.drawRect(p1.x, p1.y, p.x - p1.x, p.y - p1.y);
+                break;
+            case ROUNDRECTANGLE2D:
+                if (p != null)
+
+                {
+                    g.drawRoundRect(p1.x, p1.y, p.x - p1.x, p.y - p1.y, 10, 10);
+                }
+                p = ev.getPoint();
+                g.drawRoundRect(p1.x, p1.y, p.x - p1.x, p.y - p1.y, 10, 10);
+                break;
+            case ELLIPSE2D:
+                if (p != null)
+                {
+                    g.drawOval(p1.x, p1.y, p.x - p1.x, p.y - p1.y);
+                }
+                p = ev.getPoint();
+                g.drawOval(p1.x, p1.y, p.x - p1.x, p.y - p1.y);
+                break;
+            case ARC2D:
+                if (p != null)
+                {
+                    g.drawArc(p1.x, p1.y, p.x - p1.x, p.y - p1.y, 30, 120);
+                }
+                p = ev.getPoint();
+                g.drawArc(p1.x, p1.y, p.x - p1.x, p.y - p1.y, 30, 120);
+                break;
+            case LINE2D:
+            case POLYGON:
+                if (p != null)
+                {
+                    g.drawLine(p1.x, p1.y, p.x, p.y);
+                }
+                p = ev.getPoint();
+                g.drawLine(p1.x, p1.y, p.x, p.y);
+                break;
+            case QUADCURVE2D:
+                if (pointIndex == 1)
+                {
+                    if (p != null)
+                    {
+                        g.drawLine(p1.x, p1.y, p.x, p.y);
+                    }
+                    p = ev.getPoint();
+                    g.drawLine(p1.x, p1.y, p.x, p.y);
+                } else
+                {
+                    Point p2 = (Point) points.get(pointIndex - 2);
+                    if (p != null)
+                    {
+                        g.draw(partialShape);
+                    }
+                    p = ev.getPoint();
+                    partialShape = new QuadCurve2D.Float(p2.x, p2.y,
+                            p1.x, p1.y, p.x, p.y);
+                    g.draw(partialShape);
+                }
+                break;
+            case CUBICCURVE2D:
+                if (pointIndex == 1)
+                {
+                    if (p != null)
+                    {
+                        g.drawLine(p1.x, p1.y, p.x, p.y);
+                    }
+                    p = ev.getPoint();
+                    g.drawLine(p1.x, p1.y, p.x, p.y);
+                } else if (pointIndex == 2)
+                {
+
+                    Point p2 = (Point) points.get(pointIndex - 2);
+                    if (p != null)
+                    {
+                        g.draw(partialShape);
+                    }
+                    p = ev.getPoint();
+                    partialShape = new QuadCurve2D.Float(p2.x, p2.y,
+                            p1.x, p1.y, p.x, p.y);
+                    g.draw(partialShape);
+                } else
+                {
+                    Point p2 = (Point) points.get(pointIndex - 2);
+                    Point p3 = (Point) points.get(pointIndex - 3);
+                    if (p != null)
+                    {
+                        g.draw(partialShape);
+                    }
+                    p = ev.getPoint();
+                    partialShape = new CubicCurve2D.Float(p3.x, p3.y,
+                            p2.x, p2.y, p1.x, p1.y, p.x, p.y);
+                    g.draw(partialShape);
+                }
+                break;
+            case HEART:
+
+                if (p != null)
+                {
+                    g.draw(new C1(p1.x, p1.y, p.x - p1.x, p.y - p1.y));
+                }
+                p = ev.getPoint();
+                g.draw(new C1(p1.x, p1.y, p.x - p1.x, p.y - p1.y));
+                break;
+
+            case HOUSE:
+
+                if (p != null)
+                {
+                    g.draw(new Casa(p1.x, p1.y, p.x - p1.x, p.y - p1.y));
+                }
+                p = ev.getPoint();
+                g.draw(new Casa(p1.x, p1.y, p.x - p1.x, p.y - p1.y));
+                break;
+
+            case TRIANGLE:
+                if (p != null)
+                {
+                    g.draw(new Triangulo(p1.x, p1.y, p.x - p1.x, p.y - p1.y));
+                }
+                p = ev.getPoint();
+                g.draw(new Triangulo(p1.x, p1.y, p.x - p1.x, p.y - p1.y));
+                break;
+            case DIAMOND:
+                if (p != null)
+                {
+                    g.draw(new Diamante(p1.x, p1.y, p.x - p1.x, p.y - p1.y));
+                }
+                p = ev.getPoint();
+                g.draw(new Diamante(p1.x, p1.y, p.x - p1.x, p.y - p1.y));
+                break;
+        }
+    }
+}
---
+++ Paintv1/src/figuras/C1.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package figuras;
+
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.GeneralPath;
+import java.awt.geom.PathIterator;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+
+/**
+ *
+ * @author kenit
+ */
+public class C1 implements Shape
+{
+
+    GeneralPath path;
+    //x = 35, y = 40, w = 210, h= 110
+    public C1(float x, float y, float w, float h)
+    {
+        System.out.println("x:" + x + "\n" + "y:" + y + "\n" + "w:" + w + "\nh:"+h);
+        path = new GeneralPath();
+        //Coordenadas del corazon
+        float x0 = x + 0.5f * w; //Parte onda del corazon   
+        float y0 = y + 0.3f * h; //73    (140,73)
+        System.out.println("X0: "+x0+" Y0: "+y0); 
+        float x1 = x + 0.1f * w; //56
+        float y1 = y + 0f * h; // 40 (56,40)
+        System.out.println("X1: "+x1+" Y1: "+y1);
+        float x2 = x + 0f * w; //
+        float y2 = y + 0.6f * h;
+        System.out.println("X2: "+x2+" Y2: "+y2);
+        float x3 = x + 0.5f * w;
+        float y3 = y + 0.9f * h;
+        System.out.println("X3: "+x3+" Y3: "+y3);
+        float x4 = x + 1f * w;
+        float y4 = y + 0.6f * h;
+        System.out.println("X4: "+x4+" Y4: "+y4);
+        float x5 = x + 0.9f * w;
+        float y5 = y + 0f * h;
+        System.out.println("X5: "+x5+" Y5: "+y5);
+        path.moveTo(x0, y0);
+        path.curveTo(x1, y1, x2, y2, x3, y3);
+        path.curveTo(x4, y4, x5, y5, x0, y0);
+    }
+
+    @Override
+    public boolean contains(Rectangle2D rect)
+    {
+        return path.contains(rect);
+    }
+
+    @Override
+    public boolean contains(Point2D pd)
+    {
+        return path.contains(pd);
+    }
+    
+    @Override
+    public boolean contains(double x, double y)
+    {
+        return path.contains(x,y);
+    }
+
+    @Override
+    public boolean contains(double x, double y, double w, double h)
+    {
+        return path.contains(x,y,w,h);
+    }
+    
+    @Override
+    public Rectangle getBounds()
+    {
+        return path.getBounds();
+    }
+    
+    @Override
+    public Rectangle2D getBounds2D()
+    {
+        return path.getBounds2D();
+    }
+    
+    @Override
+    public PathIterator getPathIterator(AffineTransform at)
+    {
+        return path.getPathIterator(at);
+    }
+    
+     @Override
+    public PathIterator getPathIterator(AffineTransform at, double flatness)
+    {
+        return path.getPathIterator(at,flatness);
+    }
+
+    @Override
+    public boolean intersects(Rectangle2D rd)
+    {
+        return path.intersects(rd);
+    }
+    
+    @Override
+    public boolean intersects(double x,double y, double w, double h)
+    {
+        return path.intersects(x, y, w, h);
+    }
+
+
+    
+
+    
+
+   
+
+}
